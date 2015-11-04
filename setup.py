9e79141fea72f4c9e59f44a4e9ec66a38376d5d7
Tristan Haffenden <tristan.haffenden@intelliflo.com>
2015-11-04 12:38:13 +0000

    Adding setup.py for pip installation.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="PyWiremock",
+    version="0.1",
+    packages=["pywiremock"],
+    license="Creative Commons Attribution-Noncommercial-Share Alike license",
+    long_description=open("README.txt").read(),
+    author="Tristan Haffenden",
+    author_email="tristanehaffenden@gmail.com"
+    )
